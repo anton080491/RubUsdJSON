18f7a09159d307388b55f2b3c439be392595e36f
antonchik <antonchik1111>
2021-06-03 22:52:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+
+inputRub.addEventListener('input', () => {
+            let request = new XMLHttpRequest();
+            request.open('GET', 'js/current.json');
+
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+
+            request.addEventListener('readystatechange', function () {
+                if (request.readyState === 4 && request.status == 200) {
+                    let data = JSON.parse(request.response);
+                    inputUsd.value = inputRub.value / data.usd; // к data.usd добираемся в нащем JSON файле
+                } else {
+                    inputUsd.value = "Что-то пошло не так!";
+                }
+            });
+        });
+
+
+    // catchData()
+    //     .then(function () {
+    //         let data = JSON.parse(request.response);
+    //         inputUsd.value = inputRub.value / data.usd; // к data.usd добираемся в нащем JSON файле
+    //     })
+    //     .catch(function () {
+    //         inputUsd.value = "Что-то пошло не так!";
+    //     });
+
